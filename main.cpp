1be054f6af808ac4d1563ece1183f5a53dd4e76d
Omar <shikzadayevomar@gmail.com>
2024-10-29 10:17:00 +0400

    10.29

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+class Shape {
+private:
+    std::string name;
+public:
+    Shape(std::string n) : name(n) {}
+    const std::string& GetName() const {
+        return name;
+    }
+    virtual  std::string Form() const = 0;
+    virtual ~Shape(){};
+};
+class Triangle : public Shape {
+public:
+    Triangle(const std::string& n): Shape(n) {
+    }
+    std::string Form() const override{
+        return "Triangle";
+    }
+};
+class Rectangle : public Triangle {
+public:
+    Rectangle(const std::string& n): Triangle(n) {}
+    std::string Form() const override {
+        return "Rectangle";
+    }
+};
+
+int main() {
+
+    return 0;
+}
